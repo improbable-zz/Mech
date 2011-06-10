24991e6fac94cfa3af2e48d67c9fc6a6b3c891e7
Craig Long <craig.d.long@gmail.com>
2011-06-09 21:22:52 -0700

    dumb REPL v2

--- cmdloop.py
+++ cmdloop.py
@@ -5,5 +5,22 @@ def repl():
         evalcmd(cmd)
     print("Goodbye")
 
+def hello(arglst):
+    print("Hello!")
+
+def add(arglst):
+    argsum = 0
+    for num in arglst:
+        argsum += int(num)
+    print(argsum)
+
+cmds = {'hello': hello, 'add': add}
+
 def evalcmd(cmdstr):
-    print(cmdstr + " seen")
+    cmdlst = cmdstr.split()
+    if cmdlst[0] == 'quit':
+        return
+    elif cmdlst[0] in cmds.keys():
+        cmds[cmdlst[0]](cmdlst[1:])
+    else:
+        print("Invalid command")
